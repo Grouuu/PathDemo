d5c67553232af6cb14abe2e967dde6a13569f36a
Grouuu <orewakamidayo@hotmail.com>
2025-02-21 21:39:20 +0100

    Pimp thrust particles

--- Assets/Scripts/Particles/RectangleVolumetricParticles2D.cs
+++ Assets/Scripts/Particles/RectangleVolumetricParticles2D.cs
@@ -41,8 +41,7 @@ public class RectangleVolumetricParticles2D : MonoBehaviour
 			Particle particle = _particles[i];
 			Vector2 offset = GetOffset(particle.position, killBounds);
 
-			if (offset != Vector2.zero)
-			{
+			if (offset != Vector2.zero)	{
 				_particles[i].position = WrapParticlePosition(particle.position, offset, killBounds);
 			}
 		}
@@ -52,21 +51,17 @@ public class RectangleVolumetricParticles2D : MonoBehaviour
 
 	private Vector2 WrapParticlePosition (Vector2 particlePosition, Vector2 offset, Bounds bounds)
 	{
-		if (offset.x == 1)
-		{
+		if (offset.x == 1) {
 			particlePosition.x += bounds.size.x;
 		}
-		else if (offset.x == -1)
-		{
+		else if (offset.x == -1) {
 			particlePosition.x -= bounds.size.x;
 		}
 
-		if (offset.y == 1)
-		{
+		if (offset.y == 1) {
 			particlePosition.y += bounds.size.y;
 		}
-		else if (offset.y == -1)
-		{
+		else if (offset.y == -1) {
 			particlePosition.y -= bounds.size.y;
 		}
 
@@ -77,21 +72,17 @@ public class RectangleVolumetricParticles2D : MonoBehaviour
 	{
 		Vector2 offset = Vector2.zero;
 
-		if (position.x < bounds.min.x)
-		{
+		if (position.x < bounds.min.x) {
 			offset.x = 1;
 		}
-		else if (position.x > bounds.max.x)
-		{
+		else if (position.x > bounds.max.x)	{
 			offset.x = -1;
 		}
 
-		if (position.y < bounds.min.y)
-		{
+		if (position.y < bounds.min.y) {
 			offset.y = 1;
 		}
-		else if (position.y > bounds.max.y)
-		{
+		else if (position.y > bounds.max.y)	{
 			offset.y = -1;
 		}
 
