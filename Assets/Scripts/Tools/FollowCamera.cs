708ea319cb4f4e9c20c500ce11eaad4057a2c6d5
Grouuu <orewakamidayo@hotmail.com>
2025-02-21 22:59:48 +0100

    Fixed follow camera z override

--- Assets/Scripts/Tools/FollowCamera.cs
+++ Assets/Scripts/Tools/FollowCamera.cs
@@ -4,7 +4,7 @@ public class FollowCamera : MonoBehaviour
 {
 	private void Update ()
 	{
-		transform.position = new Vector2(Camera.main.transform.position.x, Camera.main.transform.position.y);
+		transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, transform.position.z);
 	}
 
 }
